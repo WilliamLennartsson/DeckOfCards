6162adafc7b292031f730ac3ff47d4028f7065e0
William Lennartsson <williamlennartsson@Williams-MacBook-Air.local>
2019-04-05 10:35:05 +0200

    first commit

---
+++ demo/models/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  demo
+//
+//  Created by William Lennartsson on 2019-04-05.
+//  Copyright © 2019 William Lennartsson. All rights reserved.
+//
+
+import Foundation
---
+++ demo/models/Deck.swift
@@ -0,0 +1,9 @@
+//
+//  Deck.swift
+//  demo
+//
+//  Created by William Lennartsson on 2019-04-05.
+//  Copyright © 2019 William Lennartsson. All rights reserved.
+//
+
+import Foundation
